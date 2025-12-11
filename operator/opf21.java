ee9297699afd9ff221920999be3fafa4c1d38818
niranjan <niranjan8903532@gmail.com>
2025-12-11 22:25:27 +0530

    1

--- operator/opf21.java
+++ operator/opf21.java
@@ -7,8 +7,7 @@ public class opf21
         Scanner sc= new Scanner(System.in);
         double b=sc.nextInt();
         double h=sc.nextInt();
-        double c=0.5*(b*h);
         System.out.println(c);
-
+//heloo
     }
 }
