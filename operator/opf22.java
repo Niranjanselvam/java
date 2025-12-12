c35167b57e8fc65dbdc0990ed993593a22029eb7
niranjan <niranjan8903532@gmail.com>
2025-12-12 09:24:45 +0530

    new

---
+++ operator/opf22.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+//22 Formula Based Write a Java Program to find the area of a square.
+public class opf22
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=a*a;
+        System.err.println(b);
+
+        
+    }
+}
