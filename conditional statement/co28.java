99ac2919571fe83bccd1e58fe802dc128ad7f59b
niranjan <niranjan8903532@gmail.com>
2025-12-12 23:45:46 +0530

    #

--- conditional statement/co28.java
+++ conditional statement/co28.java
@@ -1,16 +1,18 @@
-//3 Character Based Write a Java Program to check whether a character is alphabet or not.
 
 import java.util.Scanner;
 
-public class co28 {
+//3 Character Based Write a Java Program to check whether a character is alphabet or not.
+public class co28
+{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
-
-        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-            System.out.println("Alphabet.");
-        } else {
-            System.out.println("NOT an Alphabet.");
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        if(a>='a'&&a<='z'||a>='A'&&a<='Z')
+        {
+            System.out.println("alphabet");
+        }
+        else{
+            System.out.println("not");
         }
     }
 }
