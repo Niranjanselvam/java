ca1dcb8194b37dda86362d7829b2275d087873df
niranjan <niranjan8903532@gmail.com>
2025-12-13 12:57:25 +0530

    #

---
+++ operator/operator1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class oq1
+{
+    public static void main(string[] args)
+    {
+        Scanner sc=Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        system.out.println(a+b);
+    }
+}
