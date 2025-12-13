758056606b1bfdf97c8b0490fe2577cb28c3bcfa
niranjan <niranjan8903532@gmail.com>
2025-12-13 12:24:04 +0530

    #

---
+++ input and ouput/q17.java
@@ -0,0 +1,11 @@
+// Print date of birth DD/MM/YYYY
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dd = sc.next();
+        String mm = sc.next();
+        String yyyy = sc.next();
+        System.out.println(dd + "/" + mm + "/" + yyyy);
+    }
+} 
