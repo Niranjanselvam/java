557d42eb31e17a3785caab00ac6d86b455eac204
niranjan <niranjan8903532@gmail.com>
2025-12-18 18:43:45 +0530

    h

--- array/a1.java
+++ array/a1.java
@@ -1,2 +1,18 @@
 //Write a Java program to get the input from the user and print the array.
-import
+import java.util.Scanner;
+public class a1
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
